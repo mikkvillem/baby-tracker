faafeee0e9f3d64088f60c4fc73a734ed9d0fac3
mikk-sisutech <mikk.villem@sisugroup.com>
2026-01-06 14:40:03 +0200

    plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
-    preact()
+    preact(),
+    cloudflare()
+
   ],
 })
