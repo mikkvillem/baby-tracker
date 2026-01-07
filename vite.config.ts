9e514f191ec59aec3d2e698e99677aa2db1941a5
mikk-sisutech <mikk.villem@sisugroup.com>
2026-01-07 11:29:38 +0200

    enable tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite(),
-    preact(),
-    cloudflare()
-
-  ],
-})
+	plugins: [TanStackRouterVite(), preact(), cloudflare(), tailwindcss()],
+});
